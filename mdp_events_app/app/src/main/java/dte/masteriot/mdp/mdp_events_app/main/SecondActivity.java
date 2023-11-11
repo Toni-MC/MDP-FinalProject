bfeec4afa60ca725b33c6f56fb0d84db261b5e3e
mariolopez15 <mariolopezcea15@hotmail.com>
2023-11-11 12:25:22 +0100

    set event activity information

--- mdp_events_app/app/src/main/java/dte/masteriot/mdp/mdp_events_app/main/SecondActivity.java
+++ mdp_events_app/app/src/main/java/dte/masteriot/mdp/mdp_events_app/main/SecondActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -35,12 +36,21 @@ import dte.masteriot.mdp.mdp_events_app.roomDB.DatabaseClient;
 import dte.masteriot.mdp.mdp_events_app.roomDB.Favourites;
 
 public class SecondActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    long eventId = 123456789;
-    LatLng latLng;
-    String link;
-    String lugar;
+    AppDatabase db;
     Boolean favouriteSelected;
+
+
+    long id;
+    String title;
+    String description;
+    int is_free;
+    String price;
+    String dtstart;
+    String dtend;
+    String time;
+    String link;
+    String event_location;
+    LatLng latlng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,29 +64,65 @@ public class SecondActivity extends AppCompatActivity implements OnMapReadyCallb
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(""); //No title
 
+        Intent intent = getIntent();
+
+        id = intent.getLongExtra("id", 0);
+        title = intent.getStringExtra("title");
+        description = intent.getStringExtra("description");
+        is_free = intent.getIntExtra("is_free", 0);
+        price = intent.getStringExtra("price");
+        dtstart = intent.getStringExtra("dtstart");
+        dtend = intent.getStringExtra("dtend");
+        time = intent.getStringExtra("time");
+        link = intent.getStringExtra("link");
+        event_location = intent.getStringExtra("event_location");
+        latlng = new LatLng(intent.getDoubleExtra("latitude", 0), intent.getDoubleExtra("longitude", 0));
+
+        Log.d("Intent", title + " - " + is_free + " - " + price + " - " + dtstart + " - " + dtend + " - " + time + " - " + link + " - " + event_location);
+
+
+
+        ImageButton favButton = findViewById(R.id.fav_button);
         favouriteSelected = false;
+        db = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase();
+        db.databaseWriteExecutor.execute(() -> {
+            //check if the current event it is already followed by the user
+            if(db.favouriteDao().getFavouriteByEventID(id).size() > 0){
+                favouriteSelected = true;
+                favButton.setImageResource(R.drawable.favorite_selected_45);
+            }else{
+                favouriteSelected = false;
+                favButton.setImageResource(R.drawable.favourite_45);
+            }
+        });
 
-        //COMPROBAR SI ESTA ACTIVIDAD YA FUE ANTERIORMENTE SELECCIONADA -> PETICIÓN A ROOM CON EL ID
 
+        //Title
+        TextView titleTv = findViewById(R.id.title);
+        titleTv.setText(title);
+
+        //Dates
+        setDates(dtstart, dtend);
+
+        // FALTA periocity
         //TO SET PEROCITY
         TextView t = findViewById(R.id.circulo_sabado);
         t.setBackgroundResource(R.drawable.circle_diselected);
         t = findViewById(R.id.circulo_domingo);
         t.setBackgroundResource(R.drawable.circle_diselected);
 
+        //Location
+        TextView location = findViewById(R.id.lugar_dato);
+        location.setText(event_location);
+
+        //Deploy map fragment
+        showMap();
+
+        //Set imgage
         ImageView picture = findViewById(R.id.picture);
         picture.setAdjustViewBounds(true);
         Picasso.get().load("https://www.madrid.es/UnidadesDescentralizadas/DistritoMoratalaz/Actividades/2023/octubre/exposicion345.jpg").into(picture);
 
-
-        String dtStart = "2023-12-15T09:27:37Z";
-        TextView fechaIni = (TextView) findViewById(R.id.fechaIni_dato);
-        fechaIni.setText(getDate(dtStart));
-
-
-        lugar = "ETSIST, Campus sur UPM";
-        link = "https://www.etsist.upm.es/";
-        latLng = new LatLng(40.389717, -3.629415);
 
 
         Button linkButton = findViewById(R.id.linkButton);
@@ -96,7 +142,7 @@ public class SecondActivity extends AppCompatActivity implements OnMapReadyCallb
             public void onClick(View v) {
 
 
-                Uri gmmIntentUri = Uri.parse("geo:" + latLng.latitude + "," + latLng.longitude +"?q=" + latLng.latitude + "," + latLng.longitude + "Evento");
+                Uri gmmIntentUri = Uri.parse("geo:" + latlng.latitude + "," + latlng.longitude +"?q=" + latlng.latitude + "," + latlng.longitude + "Evento");
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                 mapIntent.setPackage("com.google.android.apps.maps");
                 if (mapIntent.resolveActivity(getPackageManager()) != null) {
@@ -107,46 +153,58 @@ public class SecondActivity extends AppCompatActivity implements OnMapReadyCallb
             }
         });
 
-        ImageButton favButton = findViewById(R.id.fav_button);
+
         favButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(favouriteSelected){
+                    //the event was deselected
                     favButton.setImageResource(R.drawable.favourite_45);
                     favouriteSelected = false;
-
+                    //delete from database as favourite event
                     AppDatabase db = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase();
-                    db.databaseWriteExecutor.execute(() -> db.favouriteDao().deleteFavourite(eventId));
-
-
+                    db.databaseWriteExecutor.execute(() -> db.favouriteDao().deleteFavourite(id));
 
 
                 }else{
+                    //the event was selected
                     favouriteSelected = true;
                     favButton.setImageResource(R.drawable.favorite_selected_45);
-
+                    //save on database as favourite
                     AppDatabase db = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase();
-                    db.databaseWriteExecutor.execute(() -> db.favouriteDao().insert(new Favourites(eventId, "hola")));
+                    db.databaseWriteExecutor.execute(() -> db.favouriteDao().insert(new Favourites(id, title)));
 
                 }
             }
         });
 
-
-
-
-
-        showMap();
     }
 
 
-    private String getDate(String dateTime){
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    private String setDates(String dateTimeStart, String dateTimeEnd){
+
+        SimpleDateFormat receiveFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat simpleDate =  new SimpleDateFormat("dd/MM/yyyy - HH:mm");
         String returnDate = "";
         try {
-            Date date = format.parse(dateTime);
-            SimpleDateFormat simpleDate =  new SimpleDateFormat("dd/MM/yyyy - hh:mm");
-            returnDate = simpleDate.format(date); // return in new format
+            Date dateStart = receiveFormat.parse(dateTimeStart);
+            Date dateEnd = receiveFormat.parse(dateTimeEnd);
+            if(dateStart.getDay() == dateEnd.getDay()){
+                TextView fechaIniTitle = (TextView) findViewById(R.id.fechaIni);
+                fechaIniTitle.setText(R.string.date);
+                TextView fechaIni = (TextView) findViewById(R.id.fechaIni_dato);
+                fechaIni.setText(simpleDate.format(dateStart));
+
+                findViewById(R.id.fechaFin).setVisibility(View.GONE);
+                findViewById(R.id.fechaFin_dato).setVisibility(View.GONE);
+
+            }else{
+
+                TextView fechaIni = (TextView) findViewById(R.id.fechaIni_dato);
+                fechaIni.setText(simpleDate.format(dateStart));
+                TextView fechaFin = (TextView) findViewById(R.id.fechaFin_dato);
+                fechaFin.setText(simpleDate.format(dateEnd));
+            }
 
         } catch (ParseException e) {
             e.printStackTrace();
@@ -165,8 +223,8 @@ public class SecondActivity extends AppCompatActivity implements OnMapReadyCallb
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         googleMap.clear();
-        googleMap.addMarker(new MarkerOptions().position(latLng).title("Marcador"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+        googleMap.addMarker(new MarkerOptions().position(latlng).title("Marcador"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 14));
     }
 
     @Override
