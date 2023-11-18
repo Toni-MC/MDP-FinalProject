54281826f386d4e7ed96adfd714f2a17032aa4e4
mariolopez15 <mariolopezcea15@hotmail.com>
2023-11-18 18:22:17 +0100

    setting activity created

--- mdp_events_app/app/src/main/java/dte/masteriot/mdp/mdp_events_app/main/SettingsActivity.java
+++ mdp_events_app/app/src/main/java/dte/masteriot/mdp/mdp_events_app/main/SettingsActivity.java
@@ -1,9 +1,12 @@
 package dte.masteriot.mdp.mdp_events_app.main;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.preference.PreferenceFragmentCompat;
 import dte.masteriot.mdp.mdp_events_app.R;
 
@@ -13,16 +16,22 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.settings, new SettingsFragment())
                     .commit();
         }
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.setting_toolbar);
+        setSupportActionBar(myToolbar);
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Settings"); //No title
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -30,5 +39,16 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
